707e9e1f88be836b986073c72c6fcdc013a83f21
mhyder <hydremsb@gmail.com>
2021-04-17 19:06:05 -0400

    code in CharDemo

--- CharDemo.java
+++ CharDemo.java
@@ -1,1 +1,21 @@
+public class CharDemo {
+	public static void main(String[] args) {
+		CharDemo cd = new CharDemo();
+		cd.digitTest();				
+	}
+	
+	public void digitTest() {
+		String test = "ABCabc0123456789';#!";
+		for(int k=0; k<test.length(); k++) {
+			char ch = test.charAt(k);
+			if(Character.isDigit(ch)) {
+			System.out.println(ch+" is a digit");				
+			}
+			if(Character.isAlphabetic(ch)) {
+				System.out.println(ch+" is alphabetic");
+			}
+		}
+	}
+
+}
 
